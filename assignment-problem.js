db842e54a1a0e46f03a05723731fbfef24d5b75a
Kamrul Hasan <you@example.com>
2022-02-04 22:56:46 +0600

    problem set

---
+++ assignment-problem.js
@@ -0,0 +1,7 @@
+// Problem 1: ana to vori >> function name : anaToVori
+
+// Problem 2: function name : pandaCost
+
+// Problem 3: function name : picnicBudget
+
+// Problem 4: function name : oddFriend
